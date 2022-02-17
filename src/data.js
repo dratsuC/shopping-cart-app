beacc504c44e8affac04e3fb7e9f95eb14d3ed8f
Kaiden H <dratsucc@protonmail.com>
2022-02-17 22:17:23 +1100

    created app

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Ipone',
+      price: 6900,
+      image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F2016%2F03%2FSmartphone.png&f=1&nofb=1'
+    },
+    {
+      id: '2',
+      name: 'Sabsug',
+      price: 4200,
+      image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fsmartphone%2Fsmartphone_PNG8528.png&f=1&nofb=1'
+    },
+    {
+      id: '3',
+      name: 'Gogal',
+      price: 20000,
+      image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.archos.com%2Fimg%2Fproducts%2Fsmartphones%2Fneon%2Farchos_50cneon%2Fgallery%2Farchos_50cneon-large_03.png&f=1&nofb=1'
+    },
+  ],
+};
+export default data;
